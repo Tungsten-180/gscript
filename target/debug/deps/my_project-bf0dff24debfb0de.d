24ba82a4e24f559e0d0776468d5e069dc8b4d692
Tungsten-180 <tesseract1.618@gmail.com>
2022-06-20 14:22:06 -0700

    incremental

--- target/debug/deps/my_project-bf0dff24debfb0de.d
+++ target/debug/deps/my_project-bf0dff24debfb0de.d
@@ -1,9 +1,8 @@
-/home/wllm/Documents/dev/gitgscript/gscript/target/debug/deps/my_project-bf0dff24debfb0de: src/main.rs src/gco.rs src/gscript_lib.rs src/extrude.rs src/shapes.rs
+/home/wllm/Documents/dev/gitgscript/gscript/target/debug/deps/my_project-bf0dff24debfb0de: src/main.rs src/gscript_lib.rs src/gscript_lib/gco.rs src/gscript_lib/shapes.rs
 
-/home/wllm/Documents/dev/gitgscript/gscript/target/debug/deps/my_project-bf0dff24debfb0de.d: src/main.rs src/gco.rs src/gscript_lib.rs src/extrude.rs src/shapes.rs
+/home/wllm/Documents/dev/gitgscript/gscript/target/debug/deps/my_project-bf0dff24debfb0de.d: src/main.rs src/gscript_lib.rs src/gscript_lib/gco.rs src/gscript_lib/shapes.rs
 
 src/main.rs:
-src/gco.rs:
 src/gscript_lib.rs:
-src/extrude.rs:
-src/shapes.rs:
+src/gscript_lib/gco.rs:
+src/gscript_lib/shapes.rs:
